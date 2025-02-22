a0d11826763be0160ed068a154e7d52cdfc07a1d
atharvaayir <atharvaayir@gmail.com>
2025-02-22 19:32:51 +0530

    Updated seat allocation model

--- Backend/models/seatAllocationModel.js
+++ Backend/models/seatAllocationModel.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 
 const SeatAllocationSchema = new mongoose.Schema({
     exam_id: { type: mongoose.Schema.Types.ObjectId, ref: "Exam", required: true },
-    classroom_id: { type: mongoose.Schema.Types.ObjectId, ref: "Classroom", required: true },
-    allocated_seats: [
+    allocations: [
         {
-            student_id: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
-            seat_position: { row: Number, column: Number },
+            subject_id: { type: mongoose.Schema.Types.ObjectId, ref: "Subject", required: true },
+            classroom_id: [{ type: mongoose.Schema.Types.ObjectId, ref: "Classroom", required: true }],
+            invigilator_id: { type: mongoose.Schema.Types.ObjectId, ref: "Invigilator", required: true },
+            allocated_seats: [
+                {
+                    student_id: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
+                    seat_position: { row: Number, column: Number },
+                }
+            ]
         }
     ],
 }, { timestamps: true });
